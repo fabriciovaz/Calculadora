2f499d936dbdd638800cb85123426c21f884fa37
fabricio <fabricio.fvs@gmail.com>
2018-07-05 21:54:57 -0300

    primeira versao

---
+++ app/src/main/java/digitalhouse/com/calculadora/MainCalculadora.java
@@ -0,0 +1,13 @@
+package digitalhouse.com.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCalculadora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_calculadora);
+    }
+}
